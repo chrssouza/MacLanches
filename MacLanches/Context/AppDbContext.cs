b865e540ffde627c25c084507f335d05ce718aa6
Christian Souza <chrsouza012@gmail.com>
2023-09-14 22:02:54 -0300

    Entity Framework, configuração da string de conexão, data annotations nas models e fazendo a migration.

---
+++ MacLanches/Context/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using MacLanches.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MacLanches.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {            
+        }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Lanche> Lanches { get; set; }
+
+    }
+}
