f21e42ad4f8b71c5c6db6a9bcd95340e605adc09
Christian Souza <chrsouza012@gmail.com>
2023-12-12 19:17:37 -0300

    Criação do modelo de domínio para pedidos

--- MacLanches/Context/AppDbContext.cs
+++ MacLanches/Context/AppDbContext.cs
@@ -12,6 +12,8 @@ namespace MacLanches.Context
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Lanche> Lanches { get; set; }
         public DbSet<CarrinhoCompraItem> CarrinhoCompraItens { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<PedidoDetalhe> PedidosDetalhes { get;set; }
 
     }
 }
