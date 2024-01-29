358c123efba03fc509b51963e8cf61cae4a64e4a
Christian Souza <chrsouza012@gmail.com>
2024-01-29 18:04:42 -0300

    Migrando o código da classe Startup para Program

--- MacLanches/Program.cs
+++ MacLanches/Program.cs
@@ -1,19 +1,110 @@
-using Microsoft.AspNetCore.Hosting;
+using MacLanches.Areas.Admin.Services;
+using MacLanches.Areas.Admin.Services.Interfaces;
+using MacLanches.Context;
+using MacLanches.Models;
+using MacLanches.Repositories;
+using MacLanches.Repositories.Interfaces;
+using MacLanches.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using ReflectionIT.Mvc.Paging;
 
-namespace MacLanches;
-public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+            .AddEntityFrameworkStores<AppDbContext>()
+            .AddDefaultTokenProviders();
+
+builder.Services.Configure<ConfigurationImagens>(builder.Configuration.GetSection("ConfigurationPastaImagens"));
+
+builder.Services.AddTransient<ILancheRepository, LancheRepository>();
+builder.Services.AddTransient<ICategoriaRepository, CategoriaRepository>();
+builder.Services.AddTransient<IPedidoRepository, PedidoRepository>();
+builder.Services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+builder.Services.AddScoped<IRelatorioVendasService, RelatorioVendasService>();
+builder.Services.AddScoped<IGraficoVendasService, GraficoVendasService>();
+
+builder.Services.AddAuthorization(options =>
 {
-    public static void Main(string[] args)
-    {
-        CreateHostBuilder(args)
-           .Build()
-           .Run();
-    }
+    options.AddPolicy("Admin",
+        politica =>
+        {
+            politica.RequireRole("Admin");
+        });
+});
 
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-        Host.CreateDefaultBuilder(args)
-            .ConfigureWebHostDefaults(webBuilder =>
-            {
-                webBuilder.UseStartup<Startup>();
-            });
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddScoped(c => CarrinhoCompra.GetCarrinho(c));
+
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddPaging(options =>
+{
+    options.ViewName = "Bootstrap4";
+    options.PageParameterName = "pageindex";
+});
+
+
+
+var app  = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseSession();
+
+app.UseRouting();
+
+CriarPerfisUsuarios(app);
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Admin}/{action=Index}/{id?}");
+
+
+    endpoints.MapControllerRoute(
+        name: "CategoriaFiltro",
+        pattern: "Lanche/{action}/{categoria?}",
+        defaults: new { controller = "Lanche", action = "List" });
+
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+app.Run();
+
+void CriarPerfisUsuarios(WebApplication app)
+{
+    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+    using (var scope = scopedFactory.CreateScope())
+    {
+        var service = scope.ServiceProvider.GetService<ISeedUserRoleInitial>();
+        //cria os perfis
+        service.SeedRoles();
+        //cria os usuários e atribui ao perfil
+        service.SeedUsers(); 
+    }
 }
